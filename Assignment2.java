9268c093c1fbb40d78a56cfb3cc1a8675a734467
pravinamare <96714279+pravinamare@users.noreply.github.com>
2022-01-14 00:07:50 +0530

    updated assignment2 and assignment3

--- Assignment2.java
+++ Assignment2.java
@@ -1,8 +1,13 @@
 package SeleniumBasics;
 
+import java.util.concurrent.TimeUnit;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Assignment2 {
 
@@ -15,6 +20,10 @@ public class Assignment2 {
 				
 				//Open Browser
 				WebDriver driver = new ChromeDriver();
+				
+				//Implicit Wait
+				
+				driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 				
 				driver.manage().window().maximize();
 				
@@ -34,17 +43,26 @@ public class Assignment2 {
 				driver.findElement(By.id("details-button")).click();
 				driver.findElement(By.id("proceed-link")).click();
 
-				Thread.sleep(1000);
+				//Thread.sleep(1000);
 				
 				//Pay bill >> Add new Payee
 				 
 				driver.findElement(By.linkText("Pay Bills")).click(); // locator  ------ By link Text
 				
-				Thread.sleep(1000);
+				
+				//ExplicitWait
+				
+				
+				WebDriverWait ewait1 = new WebDriverWait(driver, 10);
+				
+				ewait1.until(ExpectedConditions.urlMatches("http://zero.webappsecurity.com/bank/pay-bills.html"));
 				
 				driver.findElement(By.partialLinkText("Add New Pay")).click();   // locator  ------ By partial link Text
-		
-				Thread.sleep(1000);
+				
+				
+				WebDriverWait ewait2 = new WebDriverWait(driver, 10);
+				
+				ewait2.until(ExpectedConditions.textToBe(By.xpath("(//h2[contains(text(),'Who are you paying?')])"), "Who are you paying?"));
 				
 				
 				driver.findElement(By.xpath("//input[@id='np_new_payee_name']")).sendKeys("Pravina");   // locator  ------ By xpath
@@ -58,19 +76,17 @@ public class Assignment2 {
 				driver.findElement(By.xpath("//input[contains(@id,'add_new_payee')]")).click(); // locator  ------ By xpath with contains( )with attribute
 				
 				
-				//driver.findElement(By.xpath("//a[contains(@class,'dropdown-toggle')][2]")).click();
+				//Logout
+				driver.findElement(By.xpath("//i[@class='icon-user']")).click();
+				driver.findElement(By.id("logout_link")).click();
 				
 				
-	Thread.sleep(2000);
+	//Thread.sleep(2000);
 	
 	
-	//Close Browser
+				driver.close();
+				driver.quit();
 				
-			driver.close();
-				
-	//quit driver
-				
-	         driver.quit();
 	
 	
 	
--- Assignment3.java
+++ Assignment3.java
@@ -1,9 +1,13 @@
 package SeleniumBasics;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Assignment3 {
 
@@ -14,6 +18,9 @@ public class Assignment3 {
 		
 		//Open Browser
 		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		
 		driver.manage().window().maximize();
 		
@@ -33,17 +40,23 @@ public class Assignment3 {
 		driver.findElement(By.id("details-button")).click();
 		driver.findElement(By.id("proceed-link")).click();
 
-		Thread.sleep(1000);
 		
 		//Pay bill >> Purchase foreign currency >> Handle  Alert pop-UP
 		 
-		driver.findElement(By.linkText("Pay Bills")).click(); // locator  ------ By link Text
+		driver.findElement(By.linkText("Pay Bills")).click(); 
 		
-		Thread.sleep(1000);
+		//ExplicitWait
+		WebDriverWait ewait1 = new WebDriverWait(driver, 10);
 		
-		driver.findElement(By.linkText("Purchase Foreign Currency")).click();   // locator  ------By link Text
+		ewait1.until(ExpectedConditions.urlMatches("http://zero.webappsecurity.com/bank/pay-bills.html"));
+		
+		driver.findElement(By.linkText("Purchase Foreign Currency")).click();  
+		
+		WebDriverWait ewait2 = new WebDriverWait(driver, 10);
+		
+		ewait2.until(ExpectedConditions.textToBe(By.xpath("(//h2[contains(text(),'Purchase foreign currency cash')])"), "Purchase foreign currency cash"));
 
-		Thread.sleep(1000);
+	
 		
 		driver.findElement(By.xpath("//input[contains(@id,'purchase_cash')]")).click();
 		
@@ -58,7 +71,10 @@ public class Assignment3 {
 		webAppSecurityAlert.accept();
 		
 	
-		Thread.sleep(2000);
+		//Logout
+		driver.findElement(By.xpath("//i[@class='icon-user']")).click();
+		driver.findElement(By.id("logout_link")).click();
+		
 		
 		//Close Browser
 		
